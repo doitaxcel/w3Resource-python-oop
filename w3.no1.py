22a5fd192162b2b8f60a5989696e793674f33287
doitaxcel <148182276+doitaxcel@users.noreply.github.com>
2024-08-08 13:15:07 +0800

    w3.no1.py

--- w3.no1.py
+++ w3.no1.py
@@ -10,20 +10,25 @@ class Circle:
     def calculate_Area(self):
         return self.pi * self.radius**2
     
-    def calculate_parameter(self):
+    def calculate_perimeter(self):
         return 2 * self.pi * self.radius
 
+# const variable that holds the value of pi
 pi = float(3.14);
 
+# input section of the value of radius
 radius = float(input("Input the radius of the circle: "));
 
+# construction of Object with the provided value of pi and radius
 circle = Circle(pi, radius);
 
+# calculation section
+# for area using the method calculate_Area() in the object circle
 area = circle.calculate_Area();
+# for perimeter using the method calculate_Area() in the object circle
+perimeter = circle.calculate_perimeter();
 
-parameter = circle.calculate_parameter();
-
-print(f"The area of the cirlce is: {area} and the parameter is: {parameter}");
+print(f"The area of the cirlce is: {area} and the parameter is: {perimeter}");
 
 
 
